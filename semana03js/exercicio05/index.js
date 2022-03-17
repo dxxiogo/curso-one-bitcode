74895ce5c778c0a8dd8551bc74d2f457879b9c0b
Diogo Santana Freitas <98959411+dxxiogo@users.noreply.github.com>
2022-03-17 07:17:41 -0300

    Uso  do switch

---
+++ semana03js/exercicio05/index.js
@@ -0,0 +1,15 @@
+let username = "Ana"
+
+switch(username){
+    case "Gabriela":
+        alert("O case é Gabriela")
+    break
+    case "Ana": 
+        alert("O caso é Ana")
+    break    
+    case "Beta":
+        alert("O caso é Beta")
+    break
+    default:
+        alert("Nome não indentificado")            
+} //Depois que encontrar o primeiro case, ele vai executar todos os que vinherem depois, até encontrar o break
