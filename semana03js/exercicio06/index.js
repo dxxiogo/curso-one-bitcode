385bf5376ea8762d9ac8775779951437bb933aa5
Diogo Santana Freitas <98959411+dxxiogo@users.noreply.github.com>
2022-03-18 07:37:40 -0300

    Exercicios com switch

---
+++ semana03js/exercicio06/index.js
@@ -0,0 +1,8 @@
+let velocity = 50
+let acceleration = 5
+
+while(velocity <= 100){
+    console.log(`Estamos acelerando a ${velocity} Km/h`)
+    velocity += acceleration
+}
+alert("Velocidade zerada")
