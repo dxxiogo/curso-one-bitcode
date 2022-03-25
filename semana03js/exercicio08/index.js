1c140479e397f0ee40be053503a86c20df8bf212
Diogo Santana Freitas <98959411+dxxiogo@users.noreply.github.com>
2022-03-24 21:20:27 -0300

    exercicio de função

---
+++ semana03js/exercicio08/index.js
@@ -0,0 +1,9 @@
+console.log(speedTime(3, 45))
+console.log(diogo(50, 5))
+
+var diogo = function(velocity, acceleration){
+    return velocity + acceleration
+}
+function speedTime(hours, minutes){
+    return hours * minutes
+}
