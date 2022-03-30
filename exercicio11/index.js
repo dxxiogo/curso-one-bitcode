dfceee58a40bfcfec5cc7898eefb921e1102a1fe
Diogo Santana Freitas <98959411+dxxiogo@users.noreply.github.com>
2022-03-30 20:44:33 -0300

    Exercicios com Grid

---
+++ exercicio11/index.js
@@ -0,0 +1,3 @@
+let lifeOfCouple = [ ["Lucas e Gil", 5, [2, "Julia e Pedro"] ], [ "Julia e Verônica", 2, [1, "Gabriel"] ] ]
+
+let newSon = lifeOfCouple[0][1][1].push("Gilbert") 
---
+++ semana03js/pratica/ex011/index.js
@@ -0,0 +1,8 @@
+let inicialNumber = prompt("Digite um número para iniciar")
+let finalNumber = prompt("Digite o número que finalizará a contagem")
+let incremento = prompt("Digite o incremento:")
+
+while(inicialNumber < finalNumber){
+    console.log(inicialNumber)
+    inicialNumber ++
+}
